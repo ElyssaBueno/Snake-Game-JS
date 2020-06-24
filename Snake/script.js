d20c6cfb54c8a45e32878da6b9d6dbce6226ac0f
Maurício Junior <elyssadalva@gmail.com>
2020-06-24 19:27:43 -0300

    Jogo pronto

--- Snake/script.js
+++ Snake/script.js
@@ -7,6 +7,10 @@ snake[0] = {
  	y: 8*box
 }
 let direction = "right";
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
 	context.fillStyle = "lightgreen";
@@ -15,10 +19,16 @@ function criarBG(){
 
 function criarCobra(){
 	for(i=0; i < snake.length; i++){
-		context.fillStyle = "green";
+		context.fillStyle = "black";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+
+function drawFood(){
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, box, box); 
+}
+
 
 document.addEventListener('keydown', update);
 
@@ -51,8 +61,16 @@ function iniciarJogo(){
 		snake[0].y = 16 * box;
 	}	
 	
+	for(i = 1; i < snake.length; i++){
+		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+			clearInterval(jogo);
+			alert("Game Over");
+		}
+	}
+	
 	criarBG();
 	criarCobra();
+	drawFood();
 	
 	let snakeX = snake[0].x;
 	let snakeY =  snake[0].y;
@@ -70,7 +88,13 @@ function iniciarJogo(){
 		snakeY += box;
 	}
 
-	snake.pop();
+	if(snakeX != food.x || snakeY != food.y){
+		snake.pop();
+	}
+	else{
+	 	food.x = Math.floor(Math.random() * 15 + 1) * box;
+		food.y = Math.floor(Math.random() * 15 + 1) * box;
+	}
 	
 	let newHead = {
 		x: snakeX,
